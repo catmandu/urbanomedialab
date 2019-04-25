dc24542f20857759d3daefec91878852fd3aae6b
Omar Larrion Mercado <ojmercado.l97@gmail.com>
2019-04-24 21:06:55 -0500

    Modificaciones
    Se modifica todo estilo y se cambia la manera de mostrar las secciones

--- js/main.js
+++ js/main.js
@@ -1,15 +1,43 @@
 (function () {
     // your page initialization code here
     // the DOM will be available here
-    // var scene = document.getElementById('scene');
-    // var parallaxInstance = new Parallax(scene);
+    // var cenar = document.getElementById('cenar');
+    // var parallaxCena = new Parallax(cenar);
 
     var scenes = document.getElementsByClassName('escenas');
-    var parallaxes= [];
+    var parallaxes = [];
+    var hero = document.getElementById("her");
+    var heroHei = hero.offsetHeight;
 
     for (let index = 0; index < scenes.length; index++) {
         const element = scenes[index];
         var parallaxInst = new Parallax(element);
         parallaxes.push(parallaxInst);
     }
+
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+
+        // var top = document.documentElement.scrollTop || document.body.scrollTop;
+
+        // hero.style.height = (heroHei - top).toString().concat('px');
+
+        // if (document.body.scrollTop > 76 || document.documentElement.scrollTop > 76) {
+        //     document.getElementById("navbar").style.top = "0";
+        // } else {
+        //     document.getElementById("navbar").style.top = "-100px";
+        // }
+
+
+    }
 })();
+
+$(document).ready(function () {
+    $('#papeleo').pagepiling({
+        anchors: ['hero', 'ultimo', 'reels', 'nosotros', 'contacto'],
+        menu: '#menu',
+        scrollingSpeed: 300,
+        navigation: false
+    });
+});
