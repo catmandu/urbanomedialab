574c55aeee478e12dcca8983b59200550bd9ec26
Juan Carlos Uriarte Manzano <juanuri-c@herbalife.com>
2019-05-06 18:13:53 -0500

    modifying footer

--- originalWordPressSite/public_html/wp-content/themes/urbanomedialab/footer.php
+++ originalWordPressSite/public_html/wp-content/themes/urbanomedialab/footer.php
@@ -1,17 +1,8 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WpParallax
- */
-
+<?php 
+wp_footer(); 
 ?>
-
-<?php wp_footer(); ?>
-
+            <small>DERECHOS RESERVADOS URBANO MEDIA LAB <?=date('Y');?></small>
+        </div><!--contactanos-->
+    </div><!--papeleo-->
 </body>
 </html>
