476aa4d486457a47db5e721789f204ec676cef3a
Juan Uriarte <juriartemanzano@gmail.com>
2019-05-12 10:51:02 -0500

    updating function to assign values to inputs

--- originalWordPressSite/public_html/wp-content/themes/urbanomedialab/cm/js/main.js
+++ originalWordPressSite/public_html/wp-content/themes/urbanomedialab/cm/js/main.js
@@ -5,61 +5,32 @@
       $('input[name="languageName"]').val(languageName);
 
       var selLan = $(this).val();
-      navegacion(selLan);
-      contacto(selLan);
-      reels(selLan);
+      assignValues(selLan);
     });
 
     $(".custom-file-input").on("change", function() {
-        var fileName = $(this).val().split("\\").pop();
-        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-      });
+      var fileName = $(this).val().split("\\").pop();
+      $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
 
+    $(".menu-item").click((e) => {
+      var targ = $(e.currentTarget).data("target");
+      var ele = $("#"+targ);
 
-      $(".menu-item").click((e) => {
-        var targ = $(e.currentTarget).data("target");
-        var ele = $("#"+targ);
+      $(".area.active").removeClass("active");
+      $(".menu-item").removeClass("active");
 
-        $(".area.active").removeClass("active");
-        $(".menu-item").removeClass("active");
-
-        $(e.currentTarget).addClass("active");
-        $(ele).addClass("active");
-      })
+      $(e.currentTarget).addClass("active");
+      $(ele).addClass("active");
+    })
 })();
 
-function navegacion(lanKey){
+function assignValues(lanKey){
   var selLan = languages[lanKey];
-
-  $('input[name="home"]').val(selLan['home']);
-  $('input[name="latest"]').val(selLan['latest']);
-  $('input[name="reels"]').val(selLan['reels']);
-  $('input[name="contact"]').val(selLan['contact']);
-}
-
-function contacto(lanKey){
-  var selLan = languages[lanKey];
-
-  $('input[name="name"]').val(selLan['name']);
-  $('input[name="email"]').val(selLan['email']);
-  $('input[name="phone"]').val(selLan['phone']);
-  $('input[name="message"]').val(selLan['message']);
-  $('input[name="sendEmail"]').val(selLan['sendEmail']);
-}
-
-function reels(lanKey){
-  var selLan = languages[lanKey];
-
-  $('input[name="onlineEditing1"]').val(selLan['onlineEditing1']);
-  $('input[name="onlineEditing2"]').val(selLan['onlineEditing2']);
-  $('input[name="offlineEditing1"]').val(selLan['offlineEditing1']);
-  $('input[name="offlineEditing2"]').val(selLan['offlineEditing2']);
-  $('input[name="colorCorrection1"]').val(selLan['colorCorrection1']);
-  $('input[name="colorCorrection2"]').val(selLan['colorCorrection2']);
-  $('input[name="motionGraphics1"]').val(selLan['motionGraphics1']);
-  $('input[name="motionGraphics2"]').val(selLan['motionGraphics2']);
-  $('input[name="3DAnimation1"]').val(selLan['3DAnimation1']);
-  $('input[name="3DAnimation2"]').val(selLan['3DAnimation2']);
-  $('input[name="vfx1"]').val(selLan['vfx1']);
-  $('input[name="vfx2"]').val(selLan['vfx2']);
+  var keys = [];
+  for(var k in selLan) keys.push(k);
+  
+  $.each(keys, function(index, key){
+    $('input[name="' + key + '"]').val(selLan[key]);
+  });
 }
